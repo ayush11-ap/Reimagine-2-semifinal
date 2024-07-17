a736deb5bff25ee5a31abdbfccf0eb76222f0161
Nitesh-DK <kushwahandk@gmail.com>
2024-07-17 09:31:38 +0530

    alldone

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,19 @@ import '../css/utils.css'
 
 
 mouseFollower();
+
+
+gsap.from(".box", {
+    x: "-800" ,
+    duration: 1,
+    // ease: "back(3)",
+    scrollTrigger: {
+        start: "top 40%",
+        end: "50% 50%",
+        markers: true,
+        trigger: ".page2",
+        // scrub:true
+        toggleActions:"restart none none reverse"
+    }
+    
+})
